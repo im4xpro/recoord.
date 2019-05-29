914378eb7193ee8ea6480eb09b36ff27d914d9ec
Maximilian <m.kaiser.03@t-online.de>
2019-05-29 21:02:20 +0200

    changed name @ homepage

--- index.php
+++ index.php
@@ -1,18 +1,45 @@
 <!DOCTYPE html>
 <html lang = "de">
 <head>
-  <title>Einsatzkoordination digital - Startseite</title>
+  <title>recoord.</title>
   <meta charset = "utf-8">
   <meta name="author" content = "Maximilian Kaiser"/>
   <meta name = "description" content = "Einsatzkoordination digital bringt deinen Einsatz auf's Tablet oder den PC!"></meta>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="assets/css/bootstrap.css"/>
   <link rel="stylesheet" href="assets/css/stylesheet.css"/>
 
 </head>
 <body>
+  <nav class="navbar navbar-expand-md navbar-light bg-light">
+        <a class="navbar-brand" href="#"><img src="source/logo.jpg" style="width:3.5em; "/></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#recoordNav" aria-controls="recoordNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
+        <div class="collapse navbar-collapse" id="recoordNav">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Start</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Funktionen</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Einführung</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Nutzbarkeit</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Zukunft</a>
+            </li>
+          </ul>
+
+        </div>
+      </nav>
 <div class="container-fluid  bg-white " id="header">
-<img src="source/template.png" style="width:100%;"/>
+<img src="source/pic1-compressor.png" style="opacity:0.8; width:100%;"/>
 </div>
 
 <div class="container-fluid mt-3">
@@ -23,7 +50,7 @@
         und hört bei 'Alexa setze Kaffee auf die Einkaufsliste' auf. Das Internet bringt in jedem Lebensbereich einen noch so
         kleinen Vorteil. Aber der Sucheinsatz wird immernoch auf Papier mitgezeichnet? Wir zeichnen wirklich noch auf Karten oder
         zeichnen uns welche auf ein Whiteboard? Wieso nicht einfach das Einsatzwesen revolutionieren?
-        <font color="red">Einsatzkoordination digital</font>
+        <font color="red">recoord</font>
         bringt
         jede beliebige Karte in Verbindung mit digitalen Trupps die vom Tablet oder PC kontrolliert werden.
 
@@ -34,7 +61,7 @@
   <div class="container-fluid py-2 bg-white border-bottom">
     <p class="display-3 text-center mt-4">Die Funktionen</p><br>
       <p class="lead">
-        <font color="red">Einsatzkoordination digital</font> hat im Prinzip nur den Sinn den Einsatz besser zu strukturieren oder
+        <font color="red">recoord</font> hat im Prinzip nur den Sinn den Einsatz besser zu strukturieren oder
         einen Beispieleinsatz gut erklären zu können. Dementsprechend einfach ist die App aufgebaut:<br>
         - Karte<br/>
         - Bedienfeld<br/>
@@ -66,7 +93,7 @@
     </br>
     <div class="container-fluid py-2 bg-light text-center">
 
-        <a href="einsatzkoordination.php"><button class="btn"  style="width:20em; height:5em; background-color:red; color:yellow">Hier geht's zur Einsatzkoordination</button></a>
+        <a href="recoord.php"><button class="btn"  style="width:18em; height:4em; background-color:red; color:yellow">Hier geht's zur Einsatzkoordination</button></a>
       </br>
     </div>
   </div>
@@ -74,7 +101,7 @@
   <div class="container-fluid py-2 bg-white border-bottom">
     <p class="display-3 text-center mt-4">Nutzbarkeit</p><br>
       <p class="lead">
-        <font color="red">Einsatzkoordination digital</font> lässt sich vielseitig einsetzen der Hauptverwendungszweck besteht jedoch in der
+        <font color="red">recoord</font> lässt sich vielseitig einsetzen der Hauptverwendungszweck besteht jedoch in der
         Planung und Koordinierung von Einsätzen. Da das Programm aber sehr offen gestaltet ist lässt es sich auch nutzen um einfache Lagepläne zu erstellen.
         <br><br><font color="red">Wichtig:</font> die App ist für ein Tablet oder einen PC optimiert, auf Smartphones kann nicht die nötige Fläche aufgebracht werden.
 
@@ -86,7 +113,7 @@
   <div class="container-fluid py-2 bg-light">
     <p class="display-3 text-center mt-4">Zukunft</p><br>
       <p class="lead">
-        <font color="red">Einsatzkoordination digital</font> soll nicht nur ein "Grundgerüst" für eine Einsatzkoordination bleiben, sondern vielmehr eine
+        <font color="red">recoord</font> soll nicht nur ein "Grundgerüst" für eine Einsatzkoordination bleiben, sondern vielmehr eine
         App für Hilfsorganisationen um im Ernstfall bestens organisiert zu sein. Mit dem Vorbild GeoKAT (App für den Katastrophenschutz) sollen in Zukunft Standortangepasste Karten auswählbar sein,
         auf denen wichtige Punkte für Hilfsorganisationen markiert sind. Außerdem soll die App frei konfigurierbar sein, sprich eigene Kategorien anlegen und die Farben selbst bestimmen um eine noch
         bessere Übersicht zu haben.
